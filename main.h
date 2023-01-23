68a09549bfe62c6b2497438aee276bc21f8918fc
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-01-23 06:11:28 -0800

    Some Edite

--- main.h
+++ main.h
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
 /**
  * struct print - struct for printer functions
  * @type_arg: identifier
@@ -58,4 +57,5 @@ char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+
 #endif
